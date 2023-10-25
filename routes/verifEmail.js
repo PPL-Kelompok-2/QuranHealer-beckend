3368137e658dd705a4540104f69179490568a0e6
Faridl Mukhlashin Akbarullah <41612608+ak8ar12ullah@users.noreply.github.com>
2023-10-26 00:59:58 +0700

    code token check utils test

--- routes/verifEmail.js
+++ routes/verifEmail.js
@@ -1,75 +1,75 @@
-import jwt from "jsonwebtoken";
-import cache from "memory-cache";
-import { Users } from "../model/Data.js";
-import verificationEmail from "../utils/verificationEmail.js";
-import { codeCheck } from "../utils/codeCheck.js";
+// import jwt from "jsonwebtoken";
+// import cache from "memory-cache";
+// import { Users } from "../model/Data.js";
+// import verificationEmail from "../utils/verificationEmail.js";
+// import { codeCheck } from "../utils/codeCheck.js";
 
-const secretKey = process.env.SECRETKEY;
+// const secretKey = process.env.SECRETKEY;
 
-const verifEmail = [
-  // mendapatkan codenya
-  {
-    method: "GET",
-    path: "/verif",
-    handler: (request, h) => {
-      const token = request.headers.authorization.split(" ")[1]; // Extract the token
-      // Verify the token
-      return jwt.verify(token, secretKey, async (err, decoded) => {
-        if (err) {
-          return { error: "Invalid token" };
-        }
-        const [getUser] = await Users.getData(decoded.user_id)
-          .then((data) => {
-            return data;
-          })
-          .catch((err) => {
-            return err;
-          });
-        const result = await verificationEmail(getUser.email)
-          .then((data) => {
-            return data;
-          })
-          .catch((err) => {
-            return err;
-          });
-        return { message: "Token verified", result };
-      });
-    },
-  },
-  {
-    method: "POST",
-    path: "/verif/{code}",
-    handler: (request, h) => {
-      const token = request.headers.authorization.split(" ")[1]; // Extract the token
-      const { code } = request.params;
-      // Verify the token
-      return jwt.verify(token, secretKey, async (err, decoded) => {
-        if (err) {
-          return { error: "Invalid token" };
-        }
-        const [getUser] = await Users.getData(decoded.user_id)
-          .then((data) => {
-            return data;
-          })
-          .catch((err) => {
-            return err;
-          });
-        const codeCache = cache.get(getUser.email);
-        const result = await codeCheck(
-          code.toUpperCase(),
-          codeCache,
-          getUser.user_id
-        )
-          .then((data) => {
-            return data;
-          })
-          .catch((err) => {
-            return err;
-          });
-        return { message: "Token verified", result };
-      });
-    },
-  },
-];
+// const verifEmail = [
+//   // mendapatkan codenya
+//   {
+//     method: "GET",
+//     path: "/verif",
+//     handler: (request, h) => {
+//       const token = request.headers.authorization.split(" ")[1]; // Extract the token
+//       // Verify the token
+//       return jwt.verify(token, secretKey, async (err, decoded) => {
+//         if (err) {
+//           return { error: "Invalid token" };
+//         }
+//         const [getUser] = await Users.getData(decoded.user_id)
+//           .then((data) => {
+//             return data;
+//           })
+//           .catch((err) => {
+//             return err;
+//           });
+//         const result = await verificationEmail(getUser.email)
+//           .then((data) => {
+//             return data;
+//           })
+//           .catch((err) => {
+//             return err;
+//           });
+//         return { message: "Token verified", result };
+//       });
+//     },
+//   },
+//   {
+//     method: "POST",
+//     path: "/verif/{code}",
+//     handler: (request, h) => {
+//       const token = request.headers.authorization.split(" ")[1]; // Extract the token
+//       const { code } = request.params;
+//       // Verify the token
+//       return jwt.verify(token, secretKey, async (err, decoded) => {
+//         if (err) {
+//           return { error: "Invalid token" };
+//         }
+//         const [getUser] = await Users.getData(decoded.user_id)
+//           .then((data) => {
+//             return data;
+//           })
+//           .catch((err) => {
+//             return err;
+//           });
+//         const codeCache = cache.get(getUser.email);
+//         const result = await codeCheck(
+//           code.toUpperCase(),
+//           codeCache,
+//           getUser.user_id
+//         )
+//           .then((data) => {
+//             return data;
+//           })
+//           .catch((err) => {
+//             return err;
+//           });
+//         return { message: "Token verified", result };
+//       });
+//     },
+//   },
+// ];
 
-export default verifEmail;
+// export default verifEmail;
