d2c76251e6010b908d91a96367318537ed265373
Faridl Mukhlashin Akbarullah <41612608+ak8ar12ullah@users.noreply.github.com>
2023-11-02 15:30:25 +0700

    update homepage test for vercel

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import users from "./src/routes/user.js";
 import verifEmail from "./src/routes/verifEmail.js";
 import forgetPassword from "./src/routes/forgetPassword.js";
 import config from "./config.js";
+import homepage from "./src/routes/homepage.js";
 
 const init = async () => {
   const server = Hapi.server({
@@ -10,7 +11,7 @@ const init = async () => {
     host: config.host,
   });
 
-  server.route([...users, ...verifEmail, ...forgetPassword]);
+  server.route([...homepage, ...users, ...verifEmail, ...forgetPassword]);
 
   await server.start();
   console.log("server berjalan");
