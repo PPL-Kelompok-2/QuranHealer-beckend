aae08c0135921e760d40c56a366c80640669f759
Faridl Mukhlashin Akbarullah <41612608+ak8ar12ullah@users.noreply.github.com>
2023-12-20 14:16:04 +0700

    fix

--- src/middleware/postValidation.js
+++ src/middleware/postValidation.js
@@ -4,51 +4,66 @@ import isJson from "../utils/isJSON.js";
 export const postValidation = {
   addPost(request) {
     return new Promise((resolve, reject) => {
-      if (!isJson(request.payload)) {
-        reject(new Error("data yang dikirim bukan json"));
-      }
       const schema = Joi.object({
         username: Joi.string().max(50).min(1).required(),
         judul: Joi.string().max(100).min(1).required(),
         konten: Joi.string().min(1).required(),
       });
-      const { error, value } = schema.validate(JSON.parse(request.payload));
-      if (error) {
-        reject(error);
+      try {
+        const { error, value } = schema.validate(JSON.parse(request.payload));
+        if (error) {
+          reject(error);
+        }
+        resolve(value);
+      } catch (err) {
+        const { error, value } = schema.validate(request.payload);
+        if (error) {
+          reject(error);
+        }
+        resolve(value);
       }
-      resolve(value);
     });
   },
   editPost(request) {
     return new Promise((resolve, reject) => {
-      if (!isJson(request.payload)) {
-        reject(new Error("data yang dikirim bukan json"));
-      }
       const schema = Joi.object({
         username: Joi.string().max(50).min(1),
         judul: Joi.string().max(100).min(1),
         konten: Joi.string().min(1),
       });
-      const { error, value } = schema.validate(JSON.parse(request.payload));
-      if (error) {
-        reject(error);
+      try {
+        const { error, value } = schema.validate(JSON.parse(request.payload));
+        if (error) {
+          reject(error);
+        }
+        resolve(value);
+      } catch (err) {
+        const { error, value } = schema.validate(request.payload);
+        if (error) {
+          reject(error);
+        }
+        resolve(value);
       }
-      resolve(value);
     });
   },
   addComment(request) {
     return new Promise((resolve, reject) => {
-      if (!isJson(request.payload)) {
-        reject(new Error("data yang dikirim bukan json"));
-      }
       const schema = Joi.object({
         comment: Joi.string().min(1).max(255).required(),
       });
-      const { error, value } = schema.validate(JSON.parse(request.payload));
-      if (error) {
-        reject(error);
+      try {
+        const { error, value } = schema.validate(JSON.parse(request.payload));
+        if (error) {
+          reject(error);
+        }
+        resolve(value);
+      } catch (err) {
+        const { error, value } = schema.validate(request.payload);
+        if (error) {
+          reject(error);
+        }
+        resolve(value);
       }
-      resolve(value);
     });
   },
 };
--- src/middleware/userValidation.js
+++ src/middleware/userValidation.js
@@ -2,75 +2,94 @@ import Joi from "joi";
 import isJson from "../utils/isJSON.js";
 
 const userValidation = {
-    register(request){
-        return new Promise((resolve, reject)=>{
-            if (!isJson(request.payload)) {
-                reject(new Error("data yang dikirim bukan json"));
-              }
-              const schema = Joi.object({
-                name: Joi.string().max(50).min(1).required(),
-                email: Joi.string().email().max(50).min(3).required(),
-                password: Joi.string().max(50).min(1).required(),
-                gender: Joi.string().min(1).max(1).valid('L','P').required()
-              })
-              const {error, value} = schema.validate(JSON.parse(request.payload))
-              if (error) {
-                reject(error);
-              }
-              resolve(value);
-        })
-    },
-    login(request){
-      return new Promise((resolve, reject)=>{
-        if (!isJson(request.payload)) {
-          reject(new Error("data yang dikirim bukan json"));
-        }
-        const schema = Joi.object({
-          email: Joi.string().email().max(50).min(3).required(),
-          password: Joi.string().max(50).min(1).required()
-        })
-        const {error, value} = schema.validate(JSON.parse(request.payload))
+  register(request) {
+    return new Promise((resolve, reject) => {
+      const schema = Joi.object({
+        name: Joi.string().max(50).min(1).required(),
+        email: Joi.string().email().max(50).min(3).required(),
+        password: Joi.string().max(50).min(1).required(),
+        gender: Joi.string().min(1).max(1).valid("L", "P").required(),
+      });
+      try {
+        const { error, value } = schema.validate(JSON.parse(request.payload));
         if (error) {
           reject(error);
         }
         resolve(value);
-      })
-    },
-    userEdit(request){
-      return new Promise((resolve, reject)=>{
-        if (!isJson(request.payload)) {
-          reject(new Error("data yang dikirim bukan json"));
-        }
-        const schema = Joi.object({
-          name: Joi.string().max(50).min(1),
-          email: Joi.string().email().max(50).min(3),
-          password: Joi.string().max(50).min(1),
-          gender: Joi.string().min(1).max(1).valid('L','P')
-        })
-        const {error, value} = schema.validate(JSON.parse(request.payload))
+      } catch (err) {
+        const { error, value } = schema.validate(request.payload);
         if (error) {
           reject(error);
         }
         resolve(value);
-      })
-    },
-    password(request){
-      return new Promise((resolve, reject)=>{
-          if (!isJson(request.payload)) {
-              reject(new Error("data yang dikirim bukan json"));
-            }
-            const schema = Joi.object({
-              oldPassword: Joi.string().max(50).min(1).required(),
-              newPassword: Joi.string().max(50).min(1).required(),
-            })
-            const {error, value} = schema.validate(JSON.parse(request.payload))
-            if (error) {
-              reject(error);
-            }
-            resolve(value);
-      })
+      }
+    });
   },
-}
+  login(request) {
+    return new Promise((resolve, reject) => {
+      const schema = Joi.object({
+        email: Joi.string().email().max(50).min(3).required(),
+        password: Joi.string().max(50).min(1).required(),
+      });
+      try {
+        const { error, value } = schema.validate(JSON.parse(request.payload));
+        if (error) {
+          reject(error);
+        }
+        resolve(value);
+      } catch (err) {
+        const { error, value } = schema.validate(request.payload);
+        if (error) {
+          reject(error);
+        }
+        resolve(value);
+      }
+    });
+  },
+  userEdit(request) {
+    return new Promise((resolve, reject) => {
+      const schema = Joi.object({
+        name: Joi.string().max(50).min(1),
+        email: Joi.string().email().max(50).min(3),
+        password: Joi.string().max(50).min(1),
+        gender: Joi.string().min(1).max(1).valid("L", "P"),
+      });
+      try {
+        const { error, value } = schema.validate(JSON.parse(request.payload));
+        if (error) {
+          reject(error);
+        }
+        resolve(value);
+      } catch (err) {
+        const { error, value } = schema.validate(request.payload);
+        if (error) {
+          reject(error);
+        }
+        resolve(value);
+      }
+    });
+  },
+  password(request) {
+    return new Promise((resolve, reject) => {
+      const schema = Joi.object({
+        oldPassword: Joi.string().max(50).min(1).required(),
+        newPassword: Joi.string().max(50).min(1).required(),
+      });
+      try {
+        const { error, value } = schema.validate(JSON.parse(request.payload));
+        if (error) {
+          reject(error);
+        }
+        resolve(value);
+      } catch (err) {
+        const { error, value } = schema.validate(request.payload);
+        if (error) {
+          reject(error);
+        }
+        resolve(value);
+      }
+    });
+  },
+};
 
-
-export default userValidation
+export default userValidation;
