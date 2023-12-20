7749f3de6d62c968d714d378f7d19d69c43f532f
Faridl Mukhlashin Akbarullah <41612608+ak8ar12ullah@users.noreply.github.com>
2023-12-20 13:50:30 +0700

    update

--- model/DataDevelopment.js
+++ model/DataDevelopment.js
@@ -1,31 +1,38 @@
 import User from "./User.js";
 
-export class DataDevelopment extends User{
-    constructor(){
-        super()
-    }
-    async getAllDataUsers(){
-        try{
-        const allData = await this.pool.query(`
+export class DataDevelopment extends User {
+  constructor() {
+    super();
+  }
+  async getAllDataUsers() {
+    try {
+      const allData = await this.pool.query(`
             SELECT * FROM USERS
-        `)
-        return allData.rows;
-        }catch(err){
-            throw err
-        }
+        `);
+      console.log(allData);
+      return allData.rows;
+    } catch (err) {
+      throw err;
     }
-    async deleteData(email){
-        try{
-            await this.pool.query(`
+  }
+  async deleteData(email) {
+    try {
+      await this.pool.query(
+        `
                 DELETE FROM USERS WHERE email = $1
-            `, [email])
-            const dataEmail = await this.pool.query(`
+            `,
+        [email]
+      );
+      const dataEmail = await this.pool.query(
+        `
                 SELECT * FROM USERS WHERE email = $1
-            `, [email])
-            if(!dataEmail.rows.length) return 'Berhasil didelete'
-            throw new Error('data gagal di delete')
-        }catch(err){
-            throw err
-        }
+            `,
+        [email]
+      );
+      if (!dataEmail.rows.length) return "Berhasil didelete";
+      throw new Error("data gagal di delete");
+    } catch (err) {
+      throw err;
     }
+  }
 }
--- src/controller/verifEmailController.js
+++ src/controller/verifEmailController.js
@@ -7,101 +7,116 @@ import { codeCheck } from "../utils/codeCheck.js";
 const secretKey = process.env.SECRETKEY;
 
 const verifEmailController = {
-    async verif(request, h) {
-        const token = request.headers.authorization.split(" ")[1]; // Extract the token
-        try{
-            const dataUser = await jwt.verif(token, secretKey, async (err, decoded)=>{
-              const [rest, error] = await Logouts.cekTokenLogout(token)
-        if(error){throw error}
-                if (err){
-                    throw new Error("Invelid token")
-                }
-                return decoded
-            })
-            const [getUser] = await Users.getData(dataUser.user_id)
-            const result = await verificationEmail(getUser.email)
-            return h.response({result}).code(200)
-        }catch(err){
-            return h
-                .response({
-                    error: err.message,
-                })
-                .code(400);
-        }
-        // Verify the token
-        return jwt.verify(token, secretKey, async (err, decoded) => {
-          if (err) {
-            return { error: "Invalid token" };
+  async verif(request, h) {
+    const token = request.headers.authorization.split(" ")[1]; // Extract the token
+    try {
+      const dataUser = await jwt.verify(
+        token,
+        secretKey,
+        async (err, decoded) => {
+          const [rest, error] = await Logouts.cekTokenLogout(token);
+          if (error) {
+            throw error;
           }
-          const [getUser] = await Users.getData(decoded.user_id)
-            .then((data) => {
-              return data;
-            })
-            .catch((err) => {
-              return err;
-            });
-          const result = await verificationEmail(getUser.email)
-            .then((data) => {
-              return data;
-            })
-            .catch((err) => {
-              return err;
-            });
-          return { message: "Token verified", result };
-        });
-    },
-    async verifCode(request, h) {
-        const token = request.headers.authorization.split(" ")[1]; // Extract the token
-        const { code } = request.params;
-        try{
-            const dataUser = await jwt.verif(token, secretKey, async (err, decoded)=>{
-              const [rest, error] = await Logouts.cekTokenLogout(token)
-        if(error){throw error}
-                if(err){
-                    throw new Error("Invelid token")
-                }
-                return decoded;
-            })
-            const [getUser] = await Users.getData(dataUser.user_id)
-            const codeCache = await cache.get(getUser.email)
-            const result = await codeCheck(
-                code.toUpperCase(),
-                codeCache,
-                getUser.user_id)
-            return h.response({result}).code(200)
-        }catch(err){
-            return h
-                .response({
-                    error: err.message,
-                })
-                .code(400);
-        }
-        return jwt.verify(token, secretKey, async (err, decoded) => {
           if (err) {
-            return { error: "Invalid token" };
+            throw new Error("Invelid token");
           }
-          const [getUser] = await Users.getData(decoded.user_id)
-            .then((data) => {
-              return data;
-            })
-            .catch((err) => {
-              return err;
-            });
-          const codeCache = cache.get(getUser.email);
-          const result = await codeCheck(
-            code.toUpperCase(),
-            codeCache,
-            getUser.user_id
-          )
-            .then((data) => {
-              return data;
-            })
-            .catch((err) => {
-              return err;
-            });
-          return { message: "Token verified", result };
+          return decoded;
+        }
+      );
+      const getUser = await Users.getData(dataUser.user_id);
+      console.log(getUser);
+      const result = await verificationEmail(getUser.email);
+      return h.response({ result }).code(200);
+    } catch (err) {
+      console.error(err);
+      return h
+        .response({
+          error: err.message,
+        })
+        .code(400);
+    }
+    // Verify the token
+    return jwt.verify(token, secretKey, async (err, decoded) => {
+      if (err) {
+        return { error: "Invalid token" };
+      }
+      const [getUser] = await Users.getData(decoded.user_id)
+        .then((data) => {
+          return data;
+        })
+        .catch((err) => {
+          return err;
         });
-      },
-}
+      const result = await verificationEmail(getUser.email)
+        .then((data) => {
+          return data;
+        })
+        .catch((err) => {
+          return err;
+        });
+      return { message: "Token verified", result };
+    });
+  },
+  async verifCode(request, h) {
+    const token = request.headers.authorization.split(" ")[1]; // Extract the token
+    const { code } = request.params;
+    try {
+      const dataUser = await jwt.verify(
+        token,
+        secretKey,
+        async (err, decoded) => {
+          const [rest, error] = await Logouts.cekTokenLogout(token);
+          if (error) {
+            throw error;
+          }
+          if (err) {
+            throw new Error("Invelid token");
+          }
+          return decoded;
+        }
+      );
+      const [getUser] = await Users.getData(dataUser.user_id);
+      const codeCache = await cache.get(getUser.email);
+      const result = await codeCheck(
+        code.toUpperCase(),
+        codeCache,
+        getUser.user_id
+      );
+      return h.response({ result }).code(200);
+    } catch (err) {
+      return h
+        .response({
+          error: err.message,
+        })
+        .code(400);
+    }
+    return jwt.verify(token, secretKey, async (err, decoded) => {
+      if (err) {
+        return { error: "Invalid token" };
+      }
+      const [getUser] = await Users.getData(decoded.user_id)
+        .then((data) => {
+          return data;
+        })
+        .catch((err) => {
+          return err;
+        });
+      const codeCache = cache.get(getUser.email);
+      const result = await codeCheck(
+        code.toUpperCase(),
+        codeCache,
+        getUser.user_id
+      )
+        .then((data) => {
+          return data;
+        })
+        .catch((err) => {
+          return err;
+        });
+      return { message: "Token verified", result };
+    });
+  },
+};
 
 export default verifEmailController;
